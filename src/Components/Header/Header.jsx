19d3af9ae6e82b121d8123af0287f8f3731870bb
Ando <gayane@Ando-MacBook-Pro.local>
2025-07-21 14:43:30 +0400

    Refactor code and add eslint dependencies

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import s from './Header.module.css'
-const Header=()=>{
-  return(
+import s from "./Header.module.css";
+const Header = () => {
+  return (
     <div className={s.grid}>
-         <img src='https://marketplace.canva.com/EADaosozdz0/1/0/1600w/canva-purple-sky-profile-header-XBJ23wlhl0s.jpg'></img>
-        
+      <img
+        src="https://marketplace.canva.com/EADaosozdz0/1/0/1600w/canva-purple-sky-profile-header-XBJ23wlhl0s.jpg"
+        alt="esim inch"
+      />
     </div>
   );
-}
+};
 
 export default Header;
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,68 +1,61 @@
 import s from './Register.module.css'
 import React, {useState} from 'react'
-import { NavLink } from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
+
 const Register = () => {
-const [firstName, setFirstName] = useState('');
-const [lastName, setLastName] =useState('');
-const [username, setUsername] = useState('');
-const [password, setPassword] = useState('')
-const [error, setError] = useState('');
+    const [creds, setCreds] = useState({
+        firstName: '',
+        lastName: '',
+        username: '',
+        password: ''
+    })
+
+    const [error, setError] = useState('');
 
 
+    function submit(e) {
+        e.preventDefault()
+        const isValid = checkUsername();
+        if (!isValid) return;
 
-function submit(e){
-    e.preventDefault()
-    const isValid= checkUsername();
-    if(!isValid) return;
+        const users = localStorage.getItem('users') ? JSON.parse(localStorage.getItem('users')) : [];
 
-    const users= localStorage.getItem('users')? JSON.parse(localStorage.getItem('users')): [];
-    const newUser={
-        firstName: firstName,
-        lastName: lastName,
-        username: username,
-        password:password,
+        localStorage.setItem('users', JSON.stringify(users.concat(creds)));
+        setCreds({firstName: '', lastName: '', username: '', password: ''});
+
     }
 
-    localStorage.setItem('users', JSON.stringify(users.concat(newUser)));
+    function checkUsername(value) {
+        const users = localStorage.getItem('users') ? JSON.parse(localStorage.getItem('users')) : [];
+        const isAlredyExists = users.find(user => user.username === value);
+        if (isAlredyExists) {
+            setError('The username is already taken!');
+            return false;
+        } else {
+            setError('');
+            return true;
+        }
+    }
 
-    setFirstName('');
-    setLastName('');
-    setUsername('');
-    setPassword('');
-}
-
-function checkUsername(value){
-const users= localStorage.getItem('users')? JSON.parse(localStorage.getItem('users')) : [];
-const invalidUsername = users.find(user=> user.username === value);
-if(invalidUsername){
-    setError('The username is already taken!');
-    return false;
-}else{
-    setError('');
-    return true;
-}
-}
-
-function handleUsername(e){
-    setUsername(e.target.value);
-    checkUsername(e.target.value);
-
-}
-
-
+    function handleChange(e) {
+        setCreds({ ...creds, [e.target.name]: e.target.value});
+        checkUsername(e.target.value);
+    }
 
 
     return <div className={s.reg}>
-        
-        <div className={s.register}>Register</div><br />
-        <form className={s.form}>
+
+        <div className={s.register}>Register</div>
+        <br/>
+        <form className={s.form} onSubmit={submit}>
             <span className={s.error}>{error}</span>
-            <input value={firstName} onChange={(e)=>setFirstName(e.target.value)} placeholder='First name'></input>
-            <input  value={lastName} onChange={(e)=>setLastName(e.target.value)} placeholder='Last name'></input>
-            <input value ={username} onChange={handleUsername} type='text' placeholder='Username'></input>
-            <input value={password}  onChange={(e) => setPassword(e.target.value)} placeholder='Password' type="password"></input><br />
-            <button type="submit" onClick={submit}>Submit</button>
-            <p>Already registered? <NavLink className={s.sign} to="/signIn">sign in</NavLink> </p>
+            <input name="firstName" value={creds.firstName} onChange={handleChange} placeholder='First name'></input>
+            <input name="lastName" value={creds.lastName} onChange={handleChange} placeholder='Last name'></input>
+            <input name="username" value={creds.username} onChange={handleChange} type='text' placeholder='Username'></input>
+            <input name="password" value={creds.password} onChange={handleChange} placeholder='Password'
+                   type="password"></input><br/>
+            <button type="submit">Submit</button>
+            <p>Already registered? <NavLink className={s.sign} to="/signIn">sign in</NavLink></p>
         </form>
     </div>
 }
