b512f6d8f0220c1392bf4e7fce199e8be8fafc45
Janna Petrosyan <janna.petrosyan.2002@gmail.com>
2025-07-22 20:29:09 +0400

    adding redux

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,35 +1,16 @@
-import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import * as listSlice from "./../../store/slices/listSlice.js";
 import MyList from "./MyList/MyList";
 import s from "./List.module.css";
 const List = () => {
-  const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const list = useSelector((state) => state.list_slice.list);
   const newText = useSelector((state) => state.list_slice.newText);
 
   const userFullName = JSON.parse(localStorage.getItem("currentUser"));
   const fullName = userFullName
     ? `${userFullName.firstName} ${userFullName.lastName}`
     : "Unknown";
-
-  const AddList = () => {
-    dispatch(listSlice.addList());
-  };
-  const handleDelete = (id) => {
-    dispatch(listSlice.deleteItem(id));
-  };
-
-  const changeList = (e) => {
-    dispatch(listSlice.updateList(e.target.value));
-  };
-
-  const LogOut = () => {
-    dispatch(listSlice.logout());
-    navigate("/register");
-  };
 
   return (
     <div className={s.list}>
@@ -38,14 +19,14 @@ const List = () => {
           <span className={s.user}>{fullName}</span>
         </div>
         <textarea
-          onChange={changeList}
+          onChange={(e) => dispatch(listSlice.updateList(e.target.value))}
           placeholder="Write your doing list"
           value={newText}
         />
       </div>
-      <button onClick={AddList}>Add</button>
-      <button onClick={LogOut}>logOut</button>
-      <MyList list={list} />
+      <button onClick={() => dispatch(listSlice.addList())}>Add</button>
+      <button onClick={() => dispatch(listSlice.logout())}>logOut</button>
+      <MyList />
     </div>
   );
 };
